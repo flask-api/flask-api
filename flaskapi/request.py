0f6894b656d88161408d691c7875aee51dff9d29
Tom Christie <tom@tomchristie.com>
2014-02-22 12:26:11 +0000

    Commenting and clean up

--- flaskapi/request.py
+++ flaskapi/request.py
@@ -15,6 +15,8 @@ class APIRequest(Request):
     negotiator_class = DefaultNegotiation
     empty_data_class = MultiDict
 
+    # Request parsing...
+
     @property
     def data(self):
         if not hasattr(self, '_data'):
@@ -33,43 +35,12 @@ class APIRequest(Request):
             self._parse()
         return self._files
 
-    @property
-    def accepted_renderer(self):
-        if not hasattr(self, '_accepted_renderer'):
-            self._perform_content_negotiation()
-        return self._accepted_renderer
-
-    @property
-    def accepted_media_type(self):
-        if not hasattr(self, '_accepted_media_type'):
-            self._perform_content_negotiation()
-        return self._accepted_media_type
-
-    @property
-    def method(self):
-        if not hasattr(self, '_method'):
-            self._perform_method_overloading()
-        return self._method
-
-    @property
-    def content_type(self):
-        if not hasattr(self, '_content_type'):
-            self._perform_method_overloading()
-        return self._content_type
-
-    # TODO: content_length property
-
-    @property
-    def stream(self):
-        if not hasattr(self, '_stream'):
-            self._perform_method_overloading()
-        return self._stream
-
     def _parse(self):
-        content_length = get_content_length(self.environ)  # TODO: self.content_length
-        content_type = self.content_type
-
-        if not content_type or not content_length:
+        """
+        Parse the body of the request, using whichever parser satifies the
+        client 'Content-Type' header.
+        """
+        if not self.content_type or not self.content_length:
             self._set_empty_data()
             return
 
@@ -95,37 +66,106 @@ class APIRequest(Request):
         self._form = self._data if parser.handles_form_data else self.empty_data_class()
 
     def _get_parser_options(self):
-        return {'content_length': get_content_length(self.environ)}
+        """
+        Any additional information to pass to the parser.
+        """
+        return {'content_length': self.content_length}
 
     def _set_empty_data(self):
+        """
+        If the request does not contain data then return an empty representation.
+        """
         self._data = self.empty_data_class()
         self._form = self.empty_data_class()
         self._files = self.empty_data_class()
 
+    # Content negotiation...
+
+    @property
+    def accepted_renderer(self):
+        if not hasattr(self, '_accepted_renderer'):
+            self._perform_content_negotiation()
+        return self._accepted_renderer
+
+    @property
+    def accepted_media_type(self):
+        if not hasattr(self, '_accepted_media_type'):
+            self._perform_content_negotiation()
+        return self._accepted_media_type
+
     def _perform_content_negotiation(self):
+        """
+        Determine which of the available renderers should be used for
+        rendering the response content, based on the client 'Accept' header.
+        """
         negotiator = self.negotiator_class()
         renderers = [renderer() for renderer in self.renderer_classes]
         self._accepted_renderer, self._accepted_media_type = negotiator.select_renderer(renderers)
 
+    # Method and content type overloading.
+
+    @property
+    def method(self):
+        if not hasattr(self, '_method'):
+            self._perform_method_overloading()
+        return self._method
+
+    @property
+    def content_type(self):
+        if not hasattr(self, '_content_type'):
+            self._perform_method_overloading()
+        return self._content_type
+
+    @property
+    def content_length(self):
+        if not hasattr(self, '_content_length'):
+            self._perform_method_overloading()
+        return self._content_length
+
+    @property
+    def stream(self):
+        if not hasattr(self, '_stream'):
+            self._perform_method_overloading()
+        return self._stream
+
     def _perform_method_overloading(self):
+        """
+        Perform method and content type overloading.
+
+        Provides support for browser PUT, PATCH, DELETE & other requests,
+        by specifing a '_method' form field.
+
+        Also provides support for browser non-form requests (eg JSON),
+        by specifing '_content' and '_content_type' form fields.
+        """
         self._method = super(APIRequest, self).method
         self._stream = super(APIRequest, self).stream
         self._content_type = self.headers.get('Content-Type')
-        # TODO: set content_length
+        self._content_length = get_content_length(self.environ)
 
-        if self._content_type == 'application/x-www-form-urlencoded':
+        if (self._method == 'POST' and self._content_type == 'application/x-www-form-urlencoded'):
+            # Read the request data, then push it back onto the stream again.
             body = self.get_data()
             data = url_decode_stream(io.BytesIO(body))
             self._stream = io.BytesIO(body)
             if '_method' in data:
+                # Support browser forms with PUT, PATCH, DELETE & other methods.
                 self._method = data['_method']
             if '_content' in data and '_content_type' in data:
-                self._stream = io.BytesIO(data['_content'].encode('utf8'))
+                # Support browser forms with non-form data, such as JSON.
+                body = data['_content'].encode('utf8')
+                self._stream = io.BytesIO(body)
                 self._content_type = data['_content_type']
-                # TODO: Set content_length
+                self._content_length = len(body)
+
+    # Misc...
 
     @property
     def full_path(self):
+        """
+        Werzueg's full_path implementation always appends '?', even when the
+        query string is empty.  Let's fix that.
+        """
         if not self.query_string:
             return self.path
         return self.path + u'?' + self.query_string
