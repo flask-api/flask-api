76db79ba5eca36946fb8aa9f130a52f925681c85
Saurabh Kumar <me@saurabh-kumar.com>
2014-07-28 16:37:11 +0530

    improvement(views): add markdown support for the browsable API
    - functionality is ripped-off from django rest framework
    
    closes: https://github.com/tomchristie/flask-api/issues/21

---
+++ flask_api/compat.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+# Markdown is optional
+try:
+    import markdown
+
+    def apply_markdown(text):
+        """
+        Simple wrapper around :func:`markdown.markdown` to set the base level
+        of '#' style headers to <h2>.
+        """
+
+        extensions = ['headerid(level=2)']
+        safe_mode = False
+        md = markdown.Markdown(extensions=extensions, safe_mode=safe_mode)
+        return md.convert(text)
+
+except ImportError:
+    apply_markdown = None
