3a78496f350c904ce64d30f422dfae8b6bc879c3
Jace Browning <jacebrowning@gmail.com>
2015-05-21 23:02:41 -0400

    Simplify test execution to restore coverage measurement

--- flask_api/tests/runtests.py
+++ flask_api/tests/runtests.py
@@ -1,10 +1,5 @@
 import unittest
-import sys
-import subprocess
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        unittest.main(module='flask_api.tests')
-    else:
-        subprocess.call([sys.executable, '-m', 'unittest', 'discover'])
+    unittest.main(module='flask_api.tests')
