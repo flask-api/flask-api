75f34c725881f735604ce34c63819fc48223bdba
Jace Browning <jacebrowning@gmail.com>
2015-05-21 22:14:51 -0400

    Fix test discovery in Python 3.4

--- flask_api/tests/runtests.py
+++ flask_api/tests/runtests.py
@@ -1,10 +1,10 @@
 import unittest
 import sys
+import subprocess
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         unittest.main(module='flask_api.tests')
     else:
-        argv = ['flask_api', 'discover', '-s', 'flask_api/tests']
-        unittest.main(argv=argv)
+        subprocess.call([sys.executable, '-m', 'unittest', 'discover'])
--- setup.py
+++ setup.py
@@ -85,6 +85,8 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP',
     ]
 )
