916647470994285e58715655f735baa451093bfc
Jace Browning <jacebrowning@gmail.com>
2021-06-04 15:01:52 -0400

    Expect method to be set

--- flask_api/__init__.py
+++ flask_api/__init__.py
@@ -1,3 +1,3 @@
 from flask_api.app import FlaskAPI
 
-__version__ = '3.0b3'
+__version__ = '3.0b4'
--- flask_api/request.py
+++ flask_api/request.py
@@ -142,10 +142,7 @@ class APIRequest(Request):
         Also provides support for browser non-form requests (eg JSON),
         by specifing '_content' and '_content_type' form fields.
         """
-        try:
-            self._method = super().method
-        except AttributeError:
-            self._method = 'GET'
+        self._method = super().method
         self._stream = super().stream
         self._content_type = self.headers.get('Content-Type')
         self._content_length = get_content_length(self.environ)
